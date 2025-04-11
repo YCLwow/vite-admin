4fe2ca22758806f28807ce9e661bd27c4af7b321
liuyichen <820471261@qq.com>
2025-04-11 11:11:27 +0800

    fix: 新增登录页

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
+
+import { fileURLToPath, URL } from 'node:url'
+import vitePluginAutoI18n from 'vite-auto-i18n-plugin';
+import { YoudaoTranslator } from 'vite-auto-i18n-plugin'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          hoistStatic: false,
+          cacheHandlers: false,
+        }
+      }
+    }),
+    vitePluginAutoI18n({
+        targetLangList: ['en', 'ko', 'ja'],
+        translator: new YoudaoTranslator({
+          appId: '3c918a75229340c7',
+          appKey: 'RCcVlreeP5Q2xoGNjFjvohwURe7CtMQN'
+        })
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
